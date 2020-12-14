bf674bbf22acacf8b57c487b132bc218d06a9b28
leandrofahur <leandrofahur@gmail.com>
2020-12-13 19:26:15 -0800

    axios get express

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 
+import axios from "axios";
+
 class App extends React.Component {
+  onPost = async () => {
+    const response = await axios.get("http://localhost:4000/");
+    document.querySelector("p").innerHTML = response.data;
+  };
+
   render() {
-    return <React.Fragment>Hello from App component</React.Fragment>;
+    return (
+      <React.Fragment>
+        <button type='submit' onClick={this.onPost}>
+          Get
+        </button>
+        <p></p>
+      </React.Fragment>
+    );
   }
 }
 
